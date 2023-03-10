81ce81c907b66afb2cabd31402fbf1b568330e01
Sourav Mishra <souravm297@gmail.com>
2023-03-10 06:33:55 +0530

    Create dns-resolver.sh

---
+++ dns-resolver.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dnsv_path=/tmp/dns_validator
+
+dnsvalidator -h >/dev/null
+if [ $(echo $?) -eq 0 ];then
+    echo "DNSValidator Exists"
+    echo
+else
+    echo "Installing DNSValidator"
+    git clone https://github.com/vortexau/dnsvalidator.git -q $dnsv_path
+    pip3 install -r $dnsv_path/requirements.txt
+    python3 $dnsv_path/setup.py install
+    echo
+fi
+
+dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 100 -o /tmp/all.out --silent
+run=0
+
+while run < 50 ;do
+    dnsvalidator -tL /tmp/all.out -threads 100 -o /tmp/resolvers$run.out --silent
+    sleep 2
+done
+
+# sudo rm -r /tmp/dns_validator
